0d1f0c756361eb14ccafbbf293937ea75ddc3e3c
Imaraika <inange2013@gmail.com>
2019-07-27 14:38:16 -0700

    jquerry code of toggle the images and their description have been written

--- js/script.js
+++ js/script.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
   $("#des-ic").click(function() {
-    $(".design-showing").toggle();
-    $(".des-pic").toggle();
+    $("#des-hidden").toggle();
+    $("#image-showingAgain").toggle();
   });
+  $("#dev-ic").click(function(){
+    $("#dev-hidden").toggle();
+    $("#dev-image-showing").toggle();
+  });
+  $("#pro-ic").click(function(){
+    $("#proMgt-hidden").toggle();
+    $("#pro-image-showing").toggle();
+  });
+  $("h3").css("font-weight","bold");
+  $("#des-parg").css({"text-align": "center"});
+
 });
