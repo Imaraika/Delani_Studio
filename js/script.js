748f0118dd798b9d9469fa4d2b0acbfb6cedb56f
Imaraika <inange2013@gmail.com>
2019-07-29 04:00:09 -0700

    published to gh-pages

--- js/script.js
+++ js/script.js
@@ -14,8 +14,13 @@ $(document).ready(function() {
   $("#submitBtn").click(function(){
     $("#myForm").submit();
     alert("Thank you to reach out with us!");
-  })
-
+  });
+  $('.col-md-4').bind('mouseenter', function() {
+    $('.middle').fadeIn();
+});
+$('.col-md-4').bind('mouseleave', function() {
+    $('.middle').fadeOut();
+});
 
   $("h3").css("font-weight","bold");
   $("#des-parg").css({"text-align": "center"});
