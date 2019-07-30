38cf2f298daae3b20057a421e31fec40e2bdfa67
Florence <denuwise@gmail.com>
2019-07-30 13:29:51 +0200

    css file for section portfolio have been changed

--- js/script.js
+++ js/script.js
@@ -54,3 +54,39 @@ $("#work4").mouseout(function() {
   $(".middle4").css("visibility", "hidden");
   $("#work4").css("opacity", "18");
 });
+$("#work5").hover(function() {
+  $("#work5").css("opacity", "0.3");
+  $(".projectname5").css("opacity", "1");
+  $(".middle5").css("visibility", "visible");
+});
+$("#work5").mouseout(function() {
+  $(".middle5").css("visibility", "hidden");
+  $("#work5").css("opacity", "18");
+});
+$("#work6").hover(function() {
+  $("#work6").css("opacity", "0.3");
+  $(".projectname6").css("opacity", "1");
+  $(".middle6").css("visibility", "visible");
+});
+$("#work6").mouseout(function() {
+  $(".middle6").css("visibility", "hidden");
+  $("#work6").css("opacity", "18");
+});
+$("#work7").hover(function() {
+  $("#work7").css("opacity", "0.3");
+  $(".projectname7").css("opacity", "1");
+  $(".middle7").css("visibility", "visible");
+});
+$("#work7").mouseout(function() {
+  $(".middle7").css("visibility", "hidden");
+  $("#work7").css("opacity", "18");
+});
+$("#work8").hover(function() {
+  $("#work8").css("opacity", "0.3");
+  $(".projectname8").css("opacity", "1");
+  $(".middle8").css("visibility", "visible");
+});
+$("#work8").mouseout(function() {
+  $(".middle8").css("visibility", "hidden");
+  $("#work8").css("opacity", "18");
+});
